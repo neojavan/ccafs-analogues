17526dd99bc50b53e25103b5294d8c4a9fd3cb8e
darango.ccafs <darango.ccafs@gmail.com>
2012-01-23 13:12:33 +0000

    Update build.R (comments to identify platform)

--- package/build.R
+++ package/build.R
@@ -9,5 +9,8 @@
 #              copy.package=FALSE,
 #              unlink.target=FALSE)
 
+#compiple the package for linux/unix
 system(paste("R CMD build","analogues"))
+
+#compile the package for windows
 system(paste("R CMD build","--binary","analogues"))
